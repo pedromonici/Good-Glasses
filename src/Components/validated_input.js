e63e87d756da85dd74a1c9fe3cf4ad63aca8a007
gabrielvictorcf <gabrielvcf@outlook.com>
2022-07-01 23:54:21 -0300

    remove old client files

--- src/Components/validated_input.js
+++
@@ -1,9 +0,0 @@
-export const ValidatedInput = ({id, type, label, state, onChange}) => { 
-	return (
-		<div className="custom-field">
-			<input id={id} type={type} placeholder=" " className={state.status} onChange={onChange} value={state.value}/>
-			<label htmlFor={id} className="placeholder">{label}</label>
-			<span className="error-message" aria-live="polite">{state.error}</span>
-		</div>
-	);
-}
